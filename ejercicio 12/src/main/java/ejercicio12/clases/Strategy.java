3cd0851ddcaea4a6f80d3ea185e45fd61e947128
Agustin Gonzalez <agustingonzalez20150303@gmail.com>
2025-09-22 14:09:43 -0300

    Modifico comentarios

--- ejercicio 12/src/main/java/ejercicio12/clases/Strategy.java
+++ ejercicio 12/src/main/java/ejercicio12/clases/Strategy.java
@@ -4,6 +4,6 @@ import ejercicio12.JobDescription;
 import java.util.List;
 
 public interface Strategy {
-    // INTERFAZ PARA QUE TODOS LOS METODOS SE COMPROMETAN A DESARROLLAR EL METODO NEXT
+    // INTERFAZ PARA QUE TODAS LAS CLASES SE COMPROMETAN A DESARROLLAR EL METODO NEXT
     public JobDescription next(List<JobDescription> jobs);
 }
